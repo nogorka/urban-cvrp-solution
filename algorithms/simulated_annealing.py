fab427e7ffea9109a768c5fae2e1583bfd04215f
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-04-23 18:57:34 +0300

    finish SA algorithm

--- algorithms/simulated_annealing.py
+++ algorithms/simulated_annealing.py
@@ -1,26 +1,18 @@
 import random
 import math
+from algorithms.graph_algorithms import precompute_distances, get_graph, set_node_strct_all, get_node_all, \
+    optimize_graph_nx
+from preprocessing.input_preprocess import read_csv_to_strct, read_csv_to_dict, reorder_csv
 
 
-def distance_matrix(nodes):
-    names = list(nodes.keys())
-    distances = {}
-    for key1, coord1 in nodes.items():
-        for key2, coord2 in nodes.items():
-            if key1 not in distances:
-                distances[key1] = {}
-            distances[key1][key2] = math.sqrt((coord1[0] - coord2[0]) ** 2 + (coord1[1] - coord2[1]) ** 2)
-    return distances, names
-
-
-def simulated_annealing(dist_matrix, node_keys, initial_temp, cooling_rate, min_temp):
-    current_solution = random.sample(node_keys, len(node_keys))
-    current_cost = path_cost(current_solution, dist_matrix)
+def simulated_annealing(dist_matrix, node_index_keys, initial_temp, cooling_rate, min_temp):
+    current_solution = random.sample(list(node_index_keys.keys()), len(node_index_keys.keys()))
+    current_cost = path_cost(current_solution, dist_matrix, node_index_keys)
 
     temperature = initial_temp
     while temperature > min_temp:
         new_solution = neighbour_solution(current_solution)
-        new_cost = path_cost(new_solution, dist_matrix)
+        new_cost = path_cost(new_solution, dist_matrix, node_index_keys)
 
         if new_cost < current_cost or random.uniform(0, 1) < math.exp((current_cost - new_cost) / temperature):
             current_solution, current_cost = new_solution, new_cost
@@ -36,25 +28,42 @@ def neighbour_solution(solution):
     return solution
 
 
-def path_cost(solution, dist_matrix):
+def path_cost(route, dist_matrix, node_index_keys):
     cost = 0
-    for i in range(len(solution) - 1):
-        cost += dist_matrix[solution[i]][solution[i + 1]]
-    cost += dist_matrix[solution[-1]][solution[0]]  # Return to the start
+    for i in range(len(route) - 1):
+        cost += get_distance(i, i+1, route, dist_matrix, node_index_keys)
+    cost += get_distance(-1, 0, route, dist_matrix, node_index_keys)
     return cost
 
 
-if __name__ == "__main__":
-    # Example set of cities (could be latitude, longitude)
-    cities = {
-        'A': (0, 0),
-        'B': (1, 3),
-        'C': (4, 3),
-        'D': (5, 1),
-        'E': (7, 5)
-    }
+def get_distance(_from, to, route, dist_matrix, node_index_keys):
+    return dist_matrix[node_index_keys[route[_from]]][node_index_keys[route[to]]]
 
-    dist_matrix, city_names = distance_matrix(cities)
+
+if __name__ == "__main__":
+    city_name = "Saint Petersburg, Russia"
+    graph_filename = "../public/road_network_graph.pickle"
+    TYPE = 'dict'
+    file = '10_ex_1.csv'
+
+    city_graph = get_graph(city_name, graph_filename)
+
+    input_csv = f'../public/example_routes/{file}'
+    output_csv = f'../public/result_routes/{file}'
+
+    points = []
+    NODE_POINTS = []
+    if TYPE == 'strct':
+        points = read_csv_to_strct(input_csv)
+        NODE_POINTS = set_node_strct_all(points, city_graph)
+
+    else:
+        points = read_csv_to_dict(input_csv)
+        NODE_POINTS = get_node_all(points, city_graph)
+
+    graph_nx = optimize_graph_nx(city_graph, node_points=NODE_POINTS, type=TYPE)
+
+    dist_matrix, city_names = precompute_distances(graph_nx, NODE_POINTS, TYPE)
 
     best_solution, best_cost = simulated_annealing(dist_matrix,
                                                    city_names,
