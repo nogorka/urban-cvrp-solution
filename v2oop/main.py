b50d7c93cfd01b48b87fefc9338c7f114f1f7201
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-04-26 12:47:34 +0300

    feature: GA v2 main.py generate result for multiple files

--- v2oop/main.py
+++ v2oop/main.py
@@ -1,23 +1,31 @@
+import os
 from algorithms.graph_algorithms import get_graph, optimize_graph_nx
 from preprocessing.input_preprocess import get_all_filenames
+from v2oop.genetic_algorithm_v2 import genetic_algorithm
 from v2oop.graph import set_node_all_point_list, precompute_distances
-from v2oop.preprocess import read_csv_to_point_list
+from v2oop.preprocess import read_csv_to_point_list, reorder_save_to_csv
 
 if __name__ == '__main__':
-    city_name = "Saint Petersburg, Russia"
-    graph_filename = "../public/road_network_graph.pickle"
+    config = {
+        'city_name': "Saint Petersburg, Russia",
+        'graph_filename': "../public/road_network_graph.pickle",
+        'input_dir': "../public/example_routes/",
+        'output_dir': "../public/result_routes/",
+    }
 
-    city_graph = get_graph(city_name, graph_filename)
+    city_graph = get_graph(config['city_name'], config['graph_filename'])
     graph_nx = optimize_graph_nx(city_graph)
 
-    filenames = get_all_filenames("public/example_routes")
+    filenames = get_all_filenames(config['input_dir'])
     for file in filenames:
-
-        input_csv = f'../public/example_routes/{file}'
-        output_csv = f'../public/result_routes/{file}'
+        input_csv = os.path.join(config['input_dir'], file)
+        output_csv = os.path.join(config['output_dir'], file)
 
         points = read_csv_to_point_list(input_csv)
         set_node_all_point_list(points, city_graph)
 
         distance_matrix = precompute_distances(graph_nx, points)
 
+        best_route = genetic_algorithm(population_size=10, generations=10, points=points, matrix=distance_matrix)
+        reorder_save_to_csv(input_csv, output_csv, best_route)
+        print(f"Сохранено в файл: {output_csv}")
