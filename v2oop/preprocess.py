3e6798bbbb9a0812ac0ec8ed520107187a4318e5
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-05-02 12:20:10 +0300

    refactor: standalone TSP

--- v2oop/preprocess.py
+++ v2oop/preprocess.py
@@ -1,6 +1,10 @@
 import csv
+import os
+import networkx as nx
 import pandas as pd
 
+from algorithms.graph_algorithms import get_graph, optimize_graph_nx
+from v2oop.graph import set_node_all_point_list, precompute_distances
 from v2oop.objects.point import Point
 
 
@@ -26,3 +30,19 @@ def reorder_save_to_csv(input, output, route):
     df_reordered = df_selected.set_index('id').loc[keys].reset_index()
 
     df_reordered.to_csv(output, index=False)
+
+
+def get_meta_data(config, filename):
+    input_csv = os.path.join(config['input_dir'], filename)
+    output_csv = os.path.join(config['output_dir'], filename)
+
+    city_graph = get_graph(config['city_name'], config['graph_filename'])
+    graph_nx = optimize_graph_nx(city_graph)
+    G = nx.Graph(city_graph)
+
+    city_points = read_csv_to_point_list(input_csv)
+    set_node_all_point_list(city_points, city_graph)
+
+    distance_matrix = precompute_distances(graph_nx, city_points)
+
+    return distance_matrix, city_points, input_csv, output_csv, G
