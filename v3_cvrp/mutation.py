e4ca1c022a5cf5a33a709c952fa2f16a5af293e5
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-05-03 15:12:37 +0300

    refactor: mutations

--- v3_cvrp/mutation.py
+++ v3_cvrp/mutation.py
@@ -1,12 +1,11 @@
 import numpy as np
-import random
+from random import choice, randint
 
 
 def hybrid_mutation(individual, capacity, mutation_rate=0.1):
     if np.random.rand() < mutation_rate:
-        pool = [swap_mutation, inter_route_swap_mutation,
-                relocation_mutation, inversion_mutation]
-        mutation = random.choice(pool)
+        pool = [swap_mutation, inter_route_swap_mutation, relocation_mutation, inversion_mutation]
+        mutation = choice(pool)
         mutation(individual, capacity)
 
 
@@ -18,32 +17,33 @@ def can_add_point_to_route(point, route, capacity):
 def swap_mutation(individual, capacity=None):
     """swaps two randomly chosen points within selected route"""
 
-    route = random.choice(individual.routes)
-    if len(route.points) > 1:
-        i, j = np.random.choice(len(route.points), 2, replace=False)
+    route = choice(individual.routes)
+    if route.size > 1:
+        i, j = np.random.choice(route.size, 2, replace=False)
         route.points[i], route.points[j] = route.points[j], route.points[i]
 
 
 def inter_route_swap_mutation(individual, capacity):
     """picks two different routes from the individual and swaps one point from each"""
 
-    if len(individual.routes) > 1:
+    if individual.size > 1:
         r1, r2 = np.random.choice(individual.routes, 2, replace=False)
         if r1.points and r2.points:
-            i, j = random.choice(range(len(r1.points))), random.choice(range(len(r2.points)))
-            if can_add_point_to_route(r1.points[i], r2, capacity) and can_add_point_to_route(r2.points[j], r1, capacity):
+            i, j = choice(range(r1.size)), choice(range(r2.size))
+            can_be_added_r1 = can_add_point_to_route(r1.points[i], r2, capacity)
+            if can_be_added_r1 and can_add_point_to_route(r2.points[j], r1, capacity):
                 r1.points[i], r2.points[j] = r2.points[j], r1.points[i]
 
 
 def relocation_mutation(individual, capacity):
     """selects a point from one route and moves it to another route"""
 
-    if len(individual.routes) > 1:
-        source_route = random.choice(individual.routes)
+    if individual.size > 1:
+        source_route = choice(individual.routes)
         if source_route.points:
-            point_index = random.randint(0, len(source_route.points) - 1)
+            point_index = randint(0, source_route.size - 1)
             point = source_route.get_point(point_index)
-            target_route = random.choice(individual.routes)
+            target_route = choice(individual.routes)
             if can_add_point_to_route(point, target_route, capacity):
                 source_route.pop_point(point_index)
                 target_route.add_point(point)
@@ -52,7 +52,7 @@ def relocation_mutation(individual, capacity):
 def inversion_mutation(individual, capacity=None):
     """selects a segment within a route and invert the sequence of points in that segment"""
 
-    route = random.choice(individual.routes)
-    if len(route.points) > 1:
-        start, end = sorted(np.random.choice(len(route.points), 2, replace=False))
+    route = choice(individual.routes)
+    if route.size > 1:
+        start, end = sorted(np.random.choice(route.size, 2, replace=False))
         route.points[start:end + 1] = reversed(route.points[start:end + 1])
