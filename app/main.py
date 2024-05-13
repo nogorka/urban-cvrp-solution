565c26118a0897ecbdf33252211315e562d2b95d
Ekaterina Chernogor <chernogor_k@mail.ru>
2024-05-13 16:30:20 +0300

    update: cors origins

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://nogorka-cvrp.netlify.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
